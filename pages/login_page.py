ec6fd97bfdbd458e2af06522ade7e2233839f380
ElanLeonard <eleonard1818@gmail.com>
2026-03-22 13:53:44 +0200

    Add login tests with POM and conftest

---
+++ pages/login_page.py
@@ -0,0 +1,16 @@
+class LoginPage:
+    URL = "https://www.saucedemo.com"
+
+    def __init__(self, page):
+        self.page = page
+
+    def navigate(self):
+        self.page.goto(self.URL)
+
+    def login(self, username, password):
+        self.page.fill("#user-name", username)
+        self.page.fill("#password", password)
+        self.page.click("#login-button")
+
+    def get_error_message(self):
+        return self.page.locator(".error-message-container").inner_text()
---
+++ tests/test_login.py
@@ -0,0 +1,31 @@
+from pages.login_page import LoginPage
+
+def test_valid_login(page):
+    login = LoginPage(page)
+    login.navigate()
+    login.login("standard_user", "secret_sauce")
+    assert "inventory" in page.url
+
+def test_wrong_password(page):
+    login = LoginPage(page)
+    login.navigate()
+    login.login("standard_user", "wrong_password")
+    assert "Epic sadface" in login.get_error_message()
+
+def test_locked_out_user(page):
+    login = LoginPage(page)
+    login.navigate()
+    login.login("locked_out_user", "secret_sauce")
+    assert "locked out" in login.get_error_message()
+
+def test_empty_fields(page):
+    login = LoginPage(page)
+    login.navigate()
+    login.login("", "")
+    assert "Username is required" in login.get_error_message()
+
+def test_sql_injection(page):
+    login = LoginPage(page)
+    login.navigate()
+    login.login("' OR '1'='1", "' OR '1'='1")
+    assert "inventory" not in page.url
